191d36875460781f7219767712dce991d03d2194
Joshua Jones <joshua@general-metrics.com>
2024-12-01 23:47:56 -0800

    Check in flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Advent of Code 2024";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            simple-http-server
+          ];
+        };
+    });
+}
