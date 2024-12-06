e8aa4ae04cce6d6a3bc839a322c93e5d60306861
Joshua Jones <joshua@general-metrics.com>
2024-12-06 01:08:41 -0800

    Finish part 2

--- days/4/day-4.js
+++ days/4/day-4.js
@@ -2,21 +2,18 @@ import { loadInput, readLines } from "../../src/utils.js";
 
 export async function run(date, writer) {
   const [testErr, testInput] = await loadInput(date, "test-input-1");
-  // const [test2Err, testInput2] = await loadInput(date, "test-input-2");
   const [inputErr, input] = await loadInput(date, "input-1");
 
   const actual1 = processInput(testInput);
   const answer1 = processInput(input);
-  // const actual2 = processInput2(testInput2);
-  // const answer2 = processInput2(input);
+  const actual2 = processInput2(testInput);
+  const answer2 = processInput2(input);
 
   writer("test", "1", actual1);
   writer("answer", "1", answer1);
-  // writer("test", "2", actual2);
-  // writer("answer", "2", answer2);
+  writer("test", "2", actual2);
+  writer("answer", "2", answer2);
 }
-
-const MATCH = "XMAS";
 
 function processInput(input) {
   let result = 0;
@@ -25,9 +22,22 @@ function processInput(input) {
     .trim()
     .split("\n")
     .map((l) => l.split(""));
-  console.log(lines);
 
   const lexer = new Lexer(lines);
+  lexer.walk();
+
+  return lexer.count;
+}
+
+function processInput2(input) {
+  let result = 0;
+  const minLength = 4;
+  const lines = input
+    .trim()
+    .split("\n")
+    .map((l) => l.split(""));
+
+  const lexer = new Lexer(lines, "A", 1, "MAS");
   lexer.walk();
 
   return lexer.count;
@@ -38,9 +48,11 @@ class Lexer {
   y = 0;
   count = 0;
 
-  constructor(input) {
+  constructor(input, key = "X", spread = 4, match = "XMAS") {
     this.input = input;
-    this.claimed = new Map();
+    this.key = key;
+    this.spread = spread;
+    this.match = match;
   }
 
   walk() {
@@ -48,7 +60,7 @@ class Lexer {
       const line = this.input[this.y];
       if (!line) break;
       const char = line[this.x];
-      if (char === "X") {
+      if (char === this.key) {
         const count = this.peek();
         this.count = this.count + count;
       }
@@ -72,34 +84,49 @@ class Lexer {
   peek() {
     const horizontal = this.input[this.y];
     const vertical = this.input.map((p, i) => p[this.x]);
-    const forward = horizontal.slice(this.x, this.x + 4);
-    const backward = horizontal
-      .slice(Math.max(0, this.x - 3), this.x + 1)
-      .reverse();
-    const up = vertical.slice(Math.max(0, this.y - 3), this.y + 1).reverse();
-    const down = vertical.slice(this.y, this.y + 4);
-    const forwardDown = this.drawDiagonal([1, 1]);
-    const forwardUp = this.drawDiagonal([1, -1]);
-    const backwardDown = this.drawDiagonal([-1, 1]);
-    const backwardUp = this.drawDiagonal([-1, -1]);
+    // This is past midnight, don't judge me!
+    if (this.match === "XMAS") {
+      const slices = [];
+      const up = vertical.slice(Math.max(0, this.y - 3), this.y + 1).reverse();
+      const down = vertical.slice(this.y, this.y + 4);
+      const forward = horizontal.slice(this.x, this.x + 4);
+      const backward = horizontal
+        .slice(Math.max(0, this.x - 3), this.x + 1)
+        .reverse();
+      slices.push(forward);
+      slices.push(backward);
+      slices.push(up);
+      slices.push(down);
+      slices.push(this.drawDiagonal([1, 1]));
+      slices.push(this.drawDiagonal([1, -1]));
+      slices.push(this.drawDiagonal([-1, 1]));
+      slices.push(this.drawDiagonal([-1, -1]));
 
-    const slices = [
-      forward,
-      backward,
-      up,
-      down,
-      forwardDown,
-      forwardUp,
-      backwardDown,
-      backwardUp,
-    ].map((m) => m.join(""));
-    return slices.filter((m) => m === "XMAS").length;
+      return slices.map((m) => m.join("")).filter((m) => m === this.match)
+        .length;
+    }
+
+    const x1 = [this.checkX([-1, -1]), this.checkX([1, 1])].sort().join("");
+    const x2 = [this.checkX([-1, 1]), this.checkX([1, -1])].sort().join("");
+    return x1 === "MS" && x2 === "MS" ? 1 : 0;
+  }
+
+  checkX([run, rise]) {
+    let result = "";
+    const y = rise < 0 ? this.y - 1 : this.y + 1;
+    const x = run < 0 ? this.x - 1 : this.x + 1;
+    const line = this.input[y];
+
+    if (line && line[x]) {
+      return line[x];
+    }
+    return result;
   }
 
   drawDiagonal([run, rise]) {
     const result = [];
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < this.spread; i++) {
       const y = rise < 0 ? this.y - i : this.y + i;
       const x = run < 0 ? this.x - i : this.x + i;
       const line = this.input[y];
